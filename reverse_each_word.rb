4c16ecc12e2d003dc520d208f37caa5d236d9852
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-03-29 04:32:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(sentence)
-    sentence = string.split(" ")
-    return_array = []
-    sentence.each do|string|
-      return_array << string.reverse
-    end
-    return_array.join(" ")
+def reverse_each_word(string)
+  array = string.split(" ") #turn string into an array
+  test_array = []
+  array.collect do|string| #iterate over the array
+    test_array << string.reverse #reverse each word in the array
   end
+  test_array.join(" ")
+end
