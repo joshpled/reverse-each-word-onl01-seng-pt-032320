5e691c1fdb3283473ae4c739f6af8f234c57f8ec
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-03-29 04:32:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(" ") #turn string into an array
   test_array = []
-  array.collect do|string| #iterate over the array
+  array.each do|string| #iterate over the array
     test_array << string.reverse #reverse each word in the array
   end
   test_array.join(" ")
